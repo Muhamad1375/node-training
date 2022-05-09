7dfefed77b52052db401d918a13b8aa6a827f05d
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2022-05-09 21:40:35 +0430

    fs sync

--- app.js
+++ app.js
@@ -1,22 +1,2 @@
-const os = require('os')
+const { readFile, writeFile } = require('fs')
 
-// info about current user
-
-const user = os.userInfo()
-console.log(user)
-
-// method returns the system uptime in second
-
-
-console.log(`the system uptime is ${os.uptime()} seconds`);
-
-
-
-const current = {
-      name: os.type(),
-      release:os.release(),
-      totalMem:os.totalmem(),
-      freeMem:os.freemem()
-}
-
-console.log(current)
